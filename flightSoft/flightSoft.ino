098518ab99e1fa48eba9ade1ae90132af62b5c51
ZachGregory <123396117+ZachGregory@users.noreply.github.com>
2024-04-06 11:05:18 -0400

    Fixed no gps fix

--- flightSoft/flightSoft.ino
+++ flightSoft/flightSoft.ino
@@ -7,13 +7,13 @@
 #include <ICM_20948.h>
 #include <Servo.h>
 
-#define GPSECHO false
 #define WIRE_PORT Wire
 #define AD0_VAL 1
+#define GPSSerial Serial1
 
 uint32_t timer = millis();
 
-Adafruit_GPS GPS(&Wire);  //GPS Object
+Adafruit_GPS GPS(&GPSSerial); //GPS Object
 ICM_20948_I2C myIMU; //IMU Object
 Servo servo1; //Servo Objects
 Servo servo2;
@@ -35,13 +35,13 @@ int correction = 0;
 
 void setup() {
   Serial.begin(115200);
+  delay(1000);
 
   // GPS setup
-  GPS.begin(0x10);
+  GPS.begin(9600);
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
   GPS.sendCommand(PGCMD_ANTENNA);
-  
   // IMU setup
   WIRE_PORT.begin();
   WIRE_PORT.setClock(400000);
@@ -65,24 +65,31 @@ void setup() {
   servo2.attach(7);
   servo1.write(servoNeutral);  // Set servos to a neutral position (range of 0-270deg)
   servo2.write(servoNeutral);
-
   delay(1000);
 }
 
 void loop() {
   char c = GPS.read();
-
+  parseData();
+  
   //Wait for data to be received
-  while (GPS.longitudeDegrees == 0){
-    delay(1000);
+  while (!GPS.fix){
+    if (millis()-timer>2000){
+      Serial.println("No fix");
+      timer=millis();
+    }
     char c = GPS.read();
+    parseData();
   }
+
+  
   
   //Set confidence levels using log constrained between 0 and 1
   gpsConf = constrain(log10(GPS.speed+0.1), 0, 1);
   imuConf = 1-gpsConf;
 
   actualBearing = gpsConf*GPS.angle + imuConf*getImuHeading();
+  Serial.println(actualBearing);
 
   correction = constrain(getHeadingToWaypoint() - actualBearing, 0, 135);
   servo1.write(servoNeutral-correction);
@@ -108,4 +115,18 @@ int getHeadingToWaypoint(){
   } else{ //Left of north
     return -1*acos((-y)/sqrt(sq(x)+sq(y)));
   }
+}
+
+void parseData(){
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences!
+    // so be very wary if using OUTPUT_ALLDATA and trying to print out data
+    Serial.print(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
+    if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
+      return; // we can fail to parse a sentence in which case we should just wait for another
+  }
+}
+
 }
